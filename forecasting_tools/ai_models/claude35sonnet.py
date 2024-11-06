55cf58af0adbbf9ad2303f09cf268453d2afbf72
Ben Wilson <wilson.benjamin1000@gmail.com>
2024-11-06 17:33:38 +0000

    Switched out bot llm for claude

--- forecasting_tools/ai_models/claude35sonnet.py
+++ forecasting_tools/ai_models/claude35sonnet.py
@@ -8,8 +8,11 @@ from forecasting_tools.ai_models.model_archetypes.anthropic_text_model import (
 class Claude35Sonnet(AnthropicTextToTextModel):
     # See Anthropic Limit on the account dashboard for most up-to-date limit
     MODEL_NAME: Final[str] = "claude-3-5-sonnet-20240620"
-    REQUESTS_PER_PERIOD_LIMIT: Final[int] = 50
+    # Latest as of Nov 6 2024 is claude-2-5-sonnet-20241022
+    # Latest in general is claude-3-5-sonnet-latest
+    # See models here https://docs.anthropic.com/en/docs/about-claude/models
+    REQUESTS_PER_PERIOD_LIMIT: Final[int] = 40
     REQUEST_PERIOD_IN_SECONDS: Final[int] = 60
     TIMEOUT_TIME: Final[int] = 40
-    TOKENS_PER_PERIOD_LIMIT: Final[int] = 40000
+    TOKENS_PER_PERIOD_LIMIT: Final[int] = 35_000
     TOKEN_PERIOD_IN_SECONDS: Final[int] = 60
